5079a1cfa17f9bfd5d97ccb5521f2d9ee6725b07
aygulozlem45-web <aygulozlem45@gmail.com>
2026-04-03 02:11:52 +0300

    decorators_aygul_ozkeklik.py

--- Week04/decorators_aygul_ozkeklik.py
+++ Week04/decorators_aygul_ozkeklik.py
@@ -4,18 +4,21 @@ import functools
 
 class Performance:
     def __init__(self):
-        self.counter = 0
-        self.total_time = 0.0
-        self.total_mem = 0.0
+        self.counter = 0        
+        self.total_time = 0.0  
+        self.total_mem = 0.0    
 
     def __call__(self, func):
-       def wrapper(*args, **kwargs):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
             start = time.time()
             result = func(*args, **kwargs)
             end = time.time()
+
             self.counter += 1
             self.total_time += (end - start)
             self.total_mem += sys.getsizeof(result)
+
             return result
         return wrapper
 
