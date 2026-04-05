56a3391d02a0e8d40349caf56ed083917dc7077e
aygulozlem45-web <aygulozlem45@gmail.com>
2026-04-05 14:19:23 +0300

    Delete sequences_aygul_ozkeklik.py

--- sequences_aygul_ozkeklik.py
+++
@@ -1,8 +0,0 @@
-def remove_duplicates(seq: list) -> list:
-    return list(dict.fromkeys(seq))
-
-def list_counts(seq: list) -> dict:
-    return {x: seq.count(x) for x in set(seq)}
-
-def reverse_dict(d: dict) -> dict:
-    return {v: k for k, v in d.items()}
