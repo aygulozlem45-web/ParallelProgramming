dee8a26d3ba3f22907f733cb0c9ae56c5a258656
aygulozlem45-web <aygulozlem45@gmail.com>
2026-04-05 14:57:14 +0300

    Update functions_aygul_ozkeklik.py

--- Week04/functions_aygul_ozkeklik.py
+++ Week04/functions_aygul_ozkeklik.py
@@ -1,8 +1,10 @@
-custom_power = lambda x=0,/, e=1 : x ** e
+custom_power = lambda x, e: x ** e
 
-def custom_equation(x: int = 0 , y: int = 0 , /, a: int = 1, b: int=1 , * , c: int = 1) -> float :
-  return (x**a + y**b)/c
+def custom_equation(x, y, a, b, c):
+    return (x**a + y**b) / c
 
-def fn_w_counter() -> (int, dict[str, int]):
-  if not hasattr(fn_w_counter, "calls"):
-    fn_w_counter.calls = 0
+def fn_w_counter():
+    if not hasattr(fn_w_counter, "count"):
+        fn_w_counter.count = 0
+    fn_w_counter.count += 1
+    return fn_w_counter.count, {__name__.split('.')[-1]: fn_w_counter.count}
